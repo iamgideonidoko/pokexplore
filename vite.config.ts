b11442c314c3cba495389a62a76dc0e0bcafaf0f
Gideon Idoko <iamgideonidoko@gmail.com>
2024-09-11 18:19:52 +0100

    chore: configure dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@/": "/src/",
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 6996,
+    strictPort: true,
+    open: true,
+  },
+});
