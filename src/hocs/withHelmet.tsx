561d9cf3110da776bcbc4d6b6a7a84f9d15fcb3b
Gideon Idoko <iamgideonidoko@gmail.com>
2024-09-11 20:15:19 +0100

    feat: add hoc for setting meta info

---
+++ src/hocs/withHelmet.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+import { Helmet } from "react-helmet";
+
+const withHelmet = (
+  Component: FC,
+  { title, description }: Partial<Record<"title" | "description", string>>,
+) => {
+  const WithHelmet = () => {
+    return (
+      <>
+        <Helmet>
+          {title && <title>{title}</title>}
+          {description && <meta name="description" content={description} />}
+        </Helmet>
+        <Component />
+      </>
+    );
+  };
+  return WithHelmet;
+};
+
+export default withHelmet;
